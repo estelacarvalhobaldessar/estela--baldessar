5cb65bd5151d841bf986fbe491e057abb51a459b
Estela de C. Baldessar <ecbaldessar@gmail.com>
2026-08-04 11:42:06 -0300

    atualização

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,29 +1,36 @@
 import '../css/Header.css'
 import imgLinkedIn from '../img/linkedin.webp'
-import imgGitHub from '../img/githubwhite.png'
+import imgGitHub from '../img/github.png'
 
 
 function Header(){
     return(
         <header>
             <div className='header-text'>
-                <h1>eu sou</h1>
-                <h1>ESTELA BALDESSAR</h1>
-                <h1>DESENVOLVEDORA WEB</h1>
+                <div className='texto1'>
+                    <h1>eu sou</h1>
+                    <h1>ESTELA BALDESSAR</h1>
+                    <h1>DESENVOLVEDORA WEB</h1>
+                </div>
             </div>
-            <ul>
-                <li>
-                    <a href="https://github.com/estelacarvalhobaldessar">
-                        <img src={imgGitHub} alt="GitHub" />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/estela-baldessar/">
-                        <img src={imgLinkedIn} alt="linkedin" />
-                    </a>
-                    
-                </li>
-            </ul>
+            <div className='apresentacao'>
+                <p>Estudante de Tecnologia da Informação, apaixonada por desenvolvimento de software e criação de soluções modernas, responsivas e eficientes.</p>
+            </div>
+            <div >
+                <ul className='redes'>
+                    <li>
+                        <a href="https://github.com/estelacarvalhobaldessar" >
+                            <img src={imgGitHub} alt="GitHub" className='github' />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/estela-baldessar/">
+                            <img src={imgLinkedIn} alt="linkedin" className='linkEdin' />
+                        </a>
+                        
+                    </li>
+                </ul>
+            </div>
         </header>
     )
 }
