50612d6bff7afe7f440215d7932cc0ac8b94ea83
Estela de C. Baldessar <ecbaldessar@gmail.com>
2026-08-12 12:21:44 -0300

    atualização

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,5 +1,40 @@
+import '../css/Footer.css'
+import html from '../img/imghtml.png'
+import css from '../img/imgcss.png'
+import js from '../img/js.png'
+import php from '../img/imgphp.png'
+import react from '../img/imgreact.png'
+import git from '../img/github.png'
+
 function Footer() {
-    return <div>Footer</div>
+    return  <div className='footer'>
+
+        <div className='footer-texto'>
+            <p className='footer-titulo'>Habilidades</p>
+            <p className='footer-titulo'>Aqui estão algumas das minhas habilidades em desenvolvimento web. </p>
+        </div>
+    <div className='linguagens'>
+            <div className='img1'>
+                <img src={html} alt="" className='html' />
+            </div>
+            <div className='img1'>
+                <img src={css} alt="" className='imagens' />
+            </div>
+            <div className='img1'>
+                <img src={js} alt="" className='imagens' />
+            </div>
+            <div className='img1'>
+                <img src={php} alt="" className='imagens' />
+            </div>
+            <div className='img1'>
+                <img src={react} alt="" className='imagens' />
+            </div>
+            <div className='img1'>
+                <img src={git} alt="" className='imagens' />
+            </div>
+    </div>
+
+    </div>
 }
 
 export default Footer
--- vite-project/src/components/Projetos.jsx
+++ vite-project/src/components/Projetos.jsx
@@ -1,11 +1,12 @@
 import '../css/projetos.css'
-import imgecoleta from '../img/imgécoleta (2).png'
+import imgecoleta from '../img/imgecoleta2.png'
 import imgstarker from '../img/imgstarker.png'
+import imgdna from '../img/imgdna2.png'
 
 function Projetos() {
     return(
         <main className='teste'>
-            <div id="projetos" className="projetos">
+            <div id="projects" className="projetos">
                 <p className='projetos-texto'>Projetos</p>
                 <h1 className='projetos-titulo'>Meus projetos</h1>
                 <p className='projetos-sobre'>Aqui estão alguns dos meus projetos de desenvolvimento web. Cada projeto foi desenvolvido com atenção aos detalhes, buscando criar interfaces intuitivas e funcionais.</p>
@@ -20,12 +21,15 @@ function Projetos() {
                 </div>
                 <div className='bloco-projeto'>
                     <img src={imgstarker} alt="" className='imgstarker' />
-                    <p className='titulo-projeto'>Projeto 2</p>
-                    <p className='texto-projeto'>Descrição do projeto 2.</p>
+                    <p className='titulo-projeto1'>STARKER BIO-ROBOTIKS</p>
+                    <p className='texto-projeto'>O Starker apresenta um produto de biorrobótica desenvolvido para auxiliar na realização de tarefas, destacando sua tecnologia, funcionalidades e aplicações de forma simples e visual.
+                    </p>
                 </div>
                 <div className='bloco-projeto'>
-                    <p className='titulo-projeto'>Projeto 3</p>
-                    <p className='texto-projeto'>Descrição do projeto 3.</p>
+                    <img src={imgdna} alt="" className='imgdna' />
+                    <p className='titulo-projeto2'>DNA E RNA</p>
+                    <p className='texto-projeto'>O FreeCode é uma plataforma educativa que transforma o estudo de genética em uma experiência interativa, com conteúdos sobre DNA, RNA e bases nitrogenadas, além de quizzes e jogos para testar os conhecimentos.
+                    </p>
                 </div>
             </div>
 
